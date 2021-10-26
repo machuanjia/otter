687f149c9070656c316d00c87fac83a8e0dcc0c0
DayYoung <machuanjia@laiye.com>
2021-10-26 14:51:36 +0800

    chore: change css

--- src/routes/async.tsx
+++ src/routes/async.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-20 16:12:45
- * @LastEditTime: 2021-10-26 09:56:22
+ * @LastEditTime: 2021-10-26 14:30:25
  * @FilePath: /otter/src/routes/async.tsx
  * @Description:
  */
@@ -11,11 +11,21 @@ import { lazy } from 'react'
 
 const routes = [
   {
+    path: '/dashboard',
+    meta: {
+      key: 'Dashboard',
+      name: 'dashboard',
+      icon: 'dashboard',
+      permission: 'dashboard*',
+    },
+    component:lazy(() => import('@/views/Dashboard')),
+  },
+  {
     path: '/counter',
     meta: {
       key: 'Counter',
       name: '计数器',
-      icon: '',
+      icon: 'counter',
       permission: 'counter*',
     },
     component:lazy(() => import('@/layouts/Route')),
@@ -37,7 +47,7 @@ const routes = [
     meta: {
       key: 'Home',
       name: 'home',
-      icon: '',
+      icon: 'home',
       permission: 'home',
     },
     component: lazy(() => import('@/views/Home')),
@@ -48,7 +58,7 @@ const routes = [
     meta: {
       key: 'otterData',
       name: 'app',
-      icon: 'dashboard',
+      icon: 'data',
       permission: 'home',
       redirect: '/data/home'
     },
---
+++ src/views/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+/*
+ * @Author: D.Y.M
+ * @Date: 2021-10-26 14:27:41
+ * @LastEditTime: 2021-10-26 14:27:42
+ * @FilePath: /otter/src/views/Dashboard/index.tsx
+ * @Description: 
+ */
+const Dashboard = () => {
+    return <>dashboard</>
+}
+export default Dashboard
