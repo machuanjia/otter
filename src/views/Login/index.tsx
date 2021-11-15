d6da94fcda017b546024e3439088e109108f3655
DayYoung <machuanjia@laiye.com>
2021-11-15 09:01:15 +0800

    feat: change

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,16 +1,18 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-20 15:09:45
- * @LastEditTime: 2021-11-14 15:55:24
+ * @LastEditTime: 2021-11-15 08:58:42
  * @FilePath: /otter/src/views/Login/index.tsx
  * @Description:
  */
-
 import { Button } from 'antd'
+import { useTranslation } from 'react-i18next'
+
 const Login = () => {
+  const { t } = useTranslation()
   return (
     <div>
-      <Button>login</Button>
+      <Button>login{t('home')}</Button>
     </div>
   )
 }
